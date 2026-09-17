5a2aa76e6c54acd13b4963da9ecda82d4b773ac9
Akugom78 <akugom78@gmail.com>
2026-09-17 21:18:40 +0800

    Update user_manager to read TOML secrets from Streamlit Cloud

--- user_manager.py
+++ user_manager.py
@@ -1,6 +1,7 @@
 import os
 import yaml
 import bcrypt
+import streamlit as st
 from yaml.loader import SafeLoader
 import streamlit_authenticator as stauth
 
@@ -9,9 +10,13 @@ class UserManager:
         self.credentials_file = '.streamlit/credentials.yaml'
         
     def initialize_users(self):
-        """Initialize default users if credentials file doesn't exist"""
+        """Initialize default users if credentials file doesn't exist AND no secrets are provided"""
         if not os.path.exists(self.credentials_file):
-            # Hash the default password securely using bcrypt
+            # If running on Streamlit Cloud, st.secrets will have the credentials
+            if hasattr(st, 'secrets') and 'credentials' in st.secrets:
+                return  # Secrets exist, do not create a local default file
+                
+            # Local fallback: create default admin
             salt = bcrypt.gensalt()
             hashed_password = bcrypt.hashpw('admin123'.encode('utf-8'), salt).decode('utf-8')
             
@@ -37,7 +42,28 @@ class UserManager:
                 yaml.dump(credentials, file, default_flow_style=False)
                 
     def load_credentials(self):
-        """Load user credentials"""
+        """Load user credentials from Streamlit Secrets (Cloud) or local file"""
+        # 1. Try to load from Streamlit Cloud Secrets first (TOML format)
+        if hasattr(st, 'secrets') and 'credentials' in st.secrets:
+            return {
+                'credentials': {
+                    'usernames': {
+                        user: {
+                            'email': st.secrets['credentials']['usernames'][user]['email'],
+                            'name': st.secrets['credentials']['usernames'][user]['name'],
+                            'password': st.secrets['credentials']['usernames'][user]['password']
+                        }
+                        for user in st.secrets['credentials']['usernames']
+                    }
+                },
+                'cookie': {
+                    'expiry_days': st.secrets['cookie']['expiry_days'],
+                    'key': st.secrets['cookie']['key'],
+                    'name': st.secrets['cookie']['name']
+                }
+            }
+            
+        # 2. Fallback to local file (YAML format)
         with open(self.credentials_file, 'r') as file:
             return yaml.load(file, Loader=SafeLoader)
             
